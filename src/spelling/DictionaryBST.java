3a480a426144489c929fd55233ec61bf601defe6
Jinhai ZHOU <zhoujinhaixd@gmail.com>
2016-05-22 14:58:53 +0200

    completed DictionaryBST

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -10,8 +10,11 @@ public class DictionaryBST implements Dictionary
 {
    private TreeSet<String> dict;
 	
-    // TODO: Implement the dictionary interface using a TreeSet.  
+ 
  	// You'll need a constructor here
+   public DictionaryBST(){
+	   this.dict = new TreeSet<String>();
+   }
 	
     
     /** Add this word to the dictionary.  Convert it to lowercase first
@@ -20,22 +23,24 @@ public class DictionaryBST implements Dictionary
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+    	String low = word.toLowerCase();
+        return this.dict.add(low);
     }
 
 
     /** Return the number of words in the dictionary */
     public int size()
     {
-    	// TODO: Implement this method
-        return 0;
+    	if (this.dict != null)
+    		return this.dict.size();
+    	else
+    		return 0;
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-    	//TODO: Implement this method
-        return false;
+    	String low = s.toLowerCase();
+        return this.dict.contains(low);
     }
 
 }
